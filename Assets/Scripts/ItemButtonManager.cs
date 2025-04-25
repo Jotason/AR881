371fb5bd268cc32ad18228932b5f5a993d54723c
Jotason <90227459+Jotason@users.noreply.github.com>
2025-04-25 00:11:23 -0500

    2504250011

---
+++ Assets/Scripts/ItemButtonManager.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ItemButtonManager : MonoBehaviour
+{
+    private string itemName;
+    private string itemDescription;
+    private Sprite itemImage;
+    private GameObject item3DModel;
+
+    public string ItemName { get => itemName; set => itemName = value; }
+    public string ItemDescription { get => itemDescription; set => itemDescription = value; }
+    public Sprite ItemImage { get => itemImage; set => itemImage = value; }
+    public GameObject Item3DModel { get => item3DModel; set => item3DModel = value; }
+
+    void Start()
+    {
+        transform.GetChild(0).GetComponent<TMP_Text>().text = itemName;
+        transform.GetChild(1).GetComponent<RawImage>().texture = itemImage.texture;
+        transform.GetChild(2).GetComponent<TMP_Text>().text = itemDescription;
+
+        var button = GetComponent<Button>();
+        button.onClick.AddListener(GameManager.instance.ARPosition);
+        button.onClick.AddListener(Create3DModel);
+    }
+
+    private void Create3DModel()
+    {
+        Instantiate(item3DModel);
+    }
+
+}
